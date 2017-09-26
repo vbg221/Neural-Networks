5c92d14dd5b864c59a535b63a78f84bc68c7b95f
vbg221 <vbg221@nyu.edu>
2017-09-26 15:48:34 -0400

    All the 3 problems solved. Yet to add Alpha in the third one.

---
+++ HW2.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 19 22:08:59 2017
+
+Name : Vatsal Gopani
+N ID : N17368916
+NetID: vbg221
+
+Description : Assignment 2 in Neural Network.
+              Testing the use of Hebbian Rule. 
+
+@author: vbg22
+"""
+
+
+
+import numpy as np
+import pandas as pd
+
+dt = pd.read_csv("C:/Users/vbg22/Downloads/Pictures/PerceptronDataF17.txt", names = ["X1", "X2", "X3", "X4", "Y"], delim_whitespace = True)
+
+maxContinuousCount = 0
+loopIterator = 0
+
+#Initial Weights:
+w = [0,0,0,0,0]
+x = [0,0,0,0,0]
+
+"""
+Training the Neurons so that weight can be adjusted and be used later on for prediction
+"""
+while(maxContinuousCount != 1000):
+    #Taking input from the DataFrame.    
+    x[0] = 1
+    x[1] = dt.ix[loopIterator]["X1"]
+    x[2] = dt.ix[loopIterator]["X2"]
+    x[3] = dt.ix[loopIterator]["X3"]
+    x[4] = dt.ix[loopIterator]["X4"]
+    
+    y = dt.ix[loopIterator]["Y"]
+    
+    #Updating weights
+    w[0] = w[0] + y
+    w[1] = w[1] + (x[1] * y)
+    w[2] = w[2] + (x[2] * y)
+    w[3] = w[3] + (x[3] * y)
+    w[4] = w[4] + (x[4] * y)
+        
+    maxContinuousCount += 1  
+    loopIterator += 1
+    
+print("Obtained weights after training are : ")
+print("b  = ", w[0])
+print("w1 = ", w[1])
+print("w2 = ", w[2])
+print("w3 = ", w[3])
+print("w4 = ", w[4])
+
+"""
+Testing on the same training set to determine wheather model fits or not.
+"""
+loopIterator = 0
+yout = []
+while(loopIterator < 1000):
+    
+    x[0] = 1
+    x[1] = dt.ix[loopIterator]["X1"]
+    x[2] = dt.ix[loopIterator]["X2"]
+    x[3] = dt.ix[loopIterator]["X3"]
+    x[4] = dt.ix[loopIterator]["X4"]
+    
+    yin = np.dot(w, x)
+    #print(yin)
+    if(yin > 0):
+        yout.append(1)
+    else:
+        yout.append(-1)
+        
+    loopIterator += 1
+
+from sklearn.metrics import accuracy_score
+print("\nAcuracy on the training data : ")
+print(accuracy_score(dt["Y"], yout))
+
+print("\nNumber of nodes incorrectly classified : ")
+print((1 - accuracy_score(dt["Y"], yout)) * 1000)
---
+++ HW3_2.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 24 21:27:25 2017
+
+Name : Vatsal Gopani
+N ID : N17368916
+NetID: vbg221
+
+Description : Assignment 3.2 in Neural Network.
+              Testing the use of Multiclass Perceptron. 
+
+@author: vbg22
+"""
+
+
+import numpy as np
+import pandas as pd
+
+
+x = [[[1], [1]], [[1], [2]], [[2], [-1]], [[2], [0]], [[-1], [2]], [[-2], [1]], [[-1], [-1]], [[-2], [-2]]]
+y = [[[-1], [-1]], [[-1], [-1]], [[-1], [1]], [[-1], [1]], [[1], [-1]], [[1], [-1]], [[1], [1]], [[1], [1]]]
+a = [[], []]
+e = [[], []]
+maxContinuousCount = 0
+loopIterator = 0
+
+w = [[0, 0], [0, 0]]
+b = [[0], [0]]
+theta = 0
+#alpha = 1
+numberOfIteration = 0
+
+"""
+Function hardlim()
+input  : A column vector
+
+Output : A column vector with same dimention as input vector
+
+Description : This function takes input vector and checks each 
+              element of the vector. 
+              - If any element is greater then the theta, it gives 1. 
+              - For element less then -theta, it gives -1. 
+              - For the rest, it gives 0.
+"""
+def hardlim(a):
+    b = [[0], [0]]
+    if(a[0][0] > theta):
+        b[0][0] = 1
+    elif(a[0][0] < (-1 * theta)):
+        b[0][0] = -1
+    else:
+        b[0][0] = 0
+    
+    if(a[1][0] > theta):
+        b[1][0] = 1
+    elif(a[1][0] < (-1 * theta)):
+        b[1][0] = -1
+    else:
+        b[1][0] = 0
+    return b
+
+"""
+Function checkZ()
+inut   : A column vector
+
+output : Boolean value
+
+Description : This function checks whether the input vector has 
+              all the elements zero elements or not. It returns 
+              TRUE in case the whole vector is zero vector and 
+              FALSE otherwise.
+"""
+def checkZ(e):
+    if(e[0][0] == 0 and e[1][0] == 0):
+        return True
+    else:
+        return False
+    
+"""
+Training the Neurons so that weight can be adjusted and be used later on for prediction
+"""
+while(maxContinuousCount != 8):
+    a = np.asmatrix(hardlim(np.asmatrix(np.add(np.matmul(w, x[loopIterator]), b))))
+    e = np.asmatrix(np.add(y[loopIterator], np.asmatrix(np.dot(-1, a))))
+    
+    if(checkZ(e)):
+        maxContinuousCount += 1
+    else:
+        w = np.asmatrix(np.add(w, np.asmatrix(np.matmul(e, np.transpose(x[loopIterator])))))
+        b = np.asmatrix(np.add(b, e))
+        
+        maxContinuousCount = 0
+    
+    loopIterator = (loopIterator + 1) % 8
+    numberOfIteration += 1
+
+
+print("Weights : \n", w, "\n")
+print("Bias : \n", b, "\n")
+
+loopIterator = 0
+correct = 0
+incorrect = 0
+
+"""
+Testing on the same training set to determine wheather model fits or not.
+"""
+while(loopIterator != 8):
+    a = np.asmatrix(hardlim(np.asmatrix(np.add(np.matmul(w, x[loopIterator]), b))))
+    e = np.asmatrix(np.add(y[loopIterator], np.asmatrix(np.dot(-1, a))))
+    
+    if(checkZ(e)):
+        correct += 1
+    else:
+        incorrect += 1
+
+    loopIterator += 1
+
+print("Number of nodes classified correctly : ", correct, "\n")
+print("Number of nodes classified incorrectly : ", incorrect, "\n")
